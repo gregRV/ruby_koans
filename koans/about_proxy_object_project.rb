1f1fcf935fb39f5000dc19aa48bac63fc5182950
Greg RV <gregvarias@gmail.com>
2014-09-27 19:21:58 -0700

    ALL 282 COMPLETE! Mountains are again merely mountains.

--- koans/about_proxy_object_project.rb
+++ koans/about_proxy_object_project.rb
@@ -13,12 +13,33 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # of the Proxy class is given in the AboutProxyObjectProject koan.
 
 class Proxy
+  attr_reader :messages, :called
+
   def initialize(target_object)
     @object = target_object
-    # ADD MORE CODE HERE
+    @messages = Hash.new(0)
   end
 
-  # WRITE CODE HERE
+  def called?(method_name)
+    @messages.has_key?(method_name)
+  end
+
+  def number_of_times_called(method_name)
+    @messages[method_name.to_sym]
+  end
+
+  def messages
+    @messages.keys
+  end
+
+  def method_missing(method_name, *args, &block)
+    if @object.respond_to?(method_name.to_sym)
+      @messages[method_name.to_sym] += 1
+      @object.send(method_name.to_sym, *args, &block)
+    else
+      raise NoMethodError
+    end
+  end
 end
 
 # The proxy object should pass the following Koan:
